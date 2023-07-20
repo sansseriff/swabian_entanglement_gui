fe7bd0558292b66d22dbc696ebcd829c3cec30f6
Andrew Mueller <amueller@caltech.edu>
2023-07-20 15:26:17 -0700

    before large changes to time walk correction

--- measurements/time_walk_analysis.py
+++ measurements/time_walk_analysis.py
@@ -4,18 +4,38 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-
 class TimeWalkRunner(Action):
     def __init__(self):
         super().__init__()
-        self.add_action(TimeWalkAnalysis(20, 300000, 500, 243))
+
+        with open(
+            "./measurements/time_walk_analysis.yaml", "r", encoding="utf8"
+        ) as stream:
+            try:
+                params = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        self.add_action(
+            TimeWalkAnalysis(
+                params["analysis_time"],
+                params["t_prime_max"],
+                params["t_prime_step"],
+                params["hist_bin_number"],
+            )
+        )
+        self.enable_save(params["save_name"])
 
 
 class TimeWalkAnalysis(Action):
-    def __init__(self, int_time, t_prime_max, t_prime_step, hist_bin_number, progress_bar=True):
+    def __init__(
+        self, int_time, t_prime_max, t_prime_step, hist_bin_number, progress_bar=True
+    ):
         super().__init__()
         self.progress_bar = progress_bar
         self.int_time = int_time
+        self.t_prime_max = t_prime_max
+        self.t_prime_step = t_prime_step
         self.t_prime_bins = np.arange(1, t_prime_max, t_prime_step)
         self.hist_bin_number = hist_bin_number
         self.hist_bins = np.linspace(0, 244.5558, hist_bin_number)
@@ -40,7 +60,7 @@ class TimeWalkAnalysis(Action):
             self.init_time = current_time
 
             if self.progress_bar:
-                self.progress_bar = tqdm(total = 100)
+                self.progress_bar = tqdm(total=100)
 
             self.period = kwargs.get("period")
             if self.period is None:
@@ -48,16 +68,6 @@ class TimeWalkAnalysis(Action):
                 print("valid period not found. using stand-in value")
                 self.hist_bins = np.linspace(0, self.period, self.hist_bin_number)
 
-            # print("shape input: ", np.shape(input_1))
-            # print("shape input: ", np.shape(input_1))
-
-            # print(np.shape(diff_1))
-            # print(np.shape(hist_1))
-
-            # print(np.shape(diff_2))
-            # print(np.shape(hist_2))
-
-            # print("shape of bins: ", np.shape([self.t_prime_bins, self.hist_bins]))
             self.hist_2d_1, edges = np.histogramdd(
                 input_1.T, bins=[self.t_prime_bins, self.hist_bins]
             )
@@ -70,7 +80,7 @@ class TimeWalkAnalysis(Action):
             return {"state": "integrating"}
 
         if self.progress_bar:
-            r = round(((current_time - self.init_time)/self.int_time)*100)
+            r = round(((current_time - self.init_time) / self.int_time) * 100)
             if self.pbar_ratio != r:
                 self.pbar_ratio = r
                 self.progress_bar.update(1)
@@ -86,25 +96,33 @@ class TimeWalkAnalysis(Action):
             if self.progress_bar:
                 self.progress_bar.close()
             self.delta_time = current_time - self.init_time
+
+            t_prime_curve_1 = develope_calibration(self.hist_2d_1)
+
+
             logger.info(
                 f"     {self.n}: Finishing. Time Integrating: {round(self.delta_time,3)}"
             )
 
-            # print("shape of result: ", np.shape(self.hist_2d_1))
-            # print(
-            #     "type: ",
-            # )
-            self.fig, self.ax = plt.subplots(1, 2, figsize=(5, 5))
-            self.ax[0].imshow(self.hist_2d_1, cmap="hot", aspect="equal")
+            self.hist_2d_1 = self.hist_2d_1.tolist()
+            self.hist_2d_2 = self.hist_2d_2.tolist()
 
-            self.ax[1].imshow(self.hist_2d_2, cmap="hot", aspect="equal")
-            self.fig.savefig("test1.png")
+            # self.fig, self.ax = plt.subplots(1, 2, figsize=(5, 5))
+            # self.ax[0].imshow(self.hist_2d_1, cmap="hot", aspect="equal")
+
+            # self.ax[1].imshow(self.hist_2d_2, cmap="hot", aspect="equal")
+            # self.fig.savefig("test1.png")
 
             self.final_state = {
                 "state": "finished",
                 "name": self.n,
                 "label": self.label,
                 "delta_time": self.delta_time,
+                "hist_2d_1": self.hist_2d_1,
+                "hist_2d_2": self.hist_2d_2,
+                "period": self.period,
+                "t_prime_max": self.t_prime_max,
+                "t_prime_step": self.t_prime_step,
             }
 
             return self.final_state
